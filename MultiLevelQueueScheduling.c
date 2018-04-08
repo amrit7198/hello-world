088b6a856a60f690e8c0a4f6a0bbd85be838ed2b
amrit7198 <36197073+amrit7198@users.noreply.github.com>
2018-04-08 11:14:47 +0530

    Added method for Preemptive Priority Scheduling

--- MultiLevelQueueScheduling.c
+++ MultiLevelQueueScheduling.c
@@ -17,6 +17,63 @@ int turnaround_time;
 int *burst;								//temp burst time storage
 int *q1;
 int *q2;
+
+int preemptive()
+{
+	int temp_process_count=process_count;
+	int finished_process=0;
+	int time=0;
+	int run_id=0;
+	int new_id;
+	while(finished_process!=temp_process_count)
+	{
+		new_id=process_arrival(time);					//process arriving at current time
+		if(new_id!=0)
+		{
+			if(run_id!=0)
+			{
+				if(p1[new_id-1].priority<p1[run_id-1].priority)		//checking priority
+				{
+					delete1(run_id);
+					insert2(run_id);			//swap into queue2
+					run_id=new_id;
+					temp_process_count--;
+				}
+			}
+			else
+			{
+				run_id=new_id;					//first running process or no process was running before
+			}
+		}
+		else
+		{
+			if(queue2_size>-1 &&queue1_size==-1)
+			{
+				time=round_robin(time,1);
+				continue;
+			}
+		}
+		if(queue1_size>-1)						//if queue1 not empty
+		{
+			wait(run_id);
+			burst[run_id-1]=burst[run_id-1]-1;
+			if(burst[run_id-1]==0)					//if a process is finished
+			{
+				delete1(run_id);
+				finished_process++;
+				run_id=0;					//resetting running process id to NULL
+				if(queue1_size!=-1)
+				{
+					run_id=allocate_p();				//select new process from queue1 if not empty
+				}
+			}
+			
+		}
+		time++;		
+	}
+	return time;
+}
+
 int main()
 {
 	int temp;
